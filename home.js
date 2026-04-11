f940b22ebb4eec480536d5a1f5451f4fbfa50c63
warapatkamnirat-cyber <warapatkamnirat@gmail.com>
2026-04-11 16:12:21 +0700

    Delete home.js

--- home.js
+++
@@ -1,9 +0,0 @@
-function openMenu() {
-  document.getElementById("sideMenu").classList.add("active");
-  document.getElementById("overlay").classList.add("active");
-}
-
-function closeMenu() {
-  document.getElementById("sideMenu").classList.remove("active");
-  document.getElementById("overlay").classList.remove("active");
-}
